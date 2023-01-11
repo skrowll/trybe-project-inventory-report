29a459901842555385cab1f9a96524f65996e52d
skrowll <lucasdejesus.rocha@hotmail.com>
2023-01-11 18:21:33 -0300

    feat(Req. 1): create test function "test_cria_produto" in tests/product/test_product.py

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,28 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui!
+    product_mock = Product(
+        id=1,
+        nome_do_produto="Nicotine Polacrilex",
+        nome_da_empresa="Target Corporation",
+        data_de_fabricacao="2021-02-18",
+        data_de_validade="2023-09-17",
+        numero_de_serie="CR25 1551 4467 2549 4402 1",
+        instrucoes_de_armazenamento="instrucao 1"
+    )
+
+    assert product_mock.id == 1
+    assert type(product_mock.id) is int
+    assert product_mock.nome_do_produto == "Nicotine Polacrilex"
+    assert type(product_mock.nome_do_produto) is str
+    assert product_mock.nome_da_empresa == "Target Corporation"
+    assert type(product_mock.nome_da_empresa) is str
+    assert product_mock.data_de_fabricacao == "2021-02-18"
+    assert type(product_mock.data_de_fabricacao) is str
+    assert product_mock.data_de_validade == "2023-09-17"
+    assert type(product_mock.data_de_validade) is str
+    assert product_mock.numero_de_serie == "CR25 1551 4467 2549 4402 1"
+    assert type(product_mock.numero_de_serie) is str
+    assert product_mock.instrucoes_de_armazenamento == "instrucao 1"
+    assert type(product_mock.instrucoes_de_armazenamento) is str
