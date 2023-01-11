a5158fb16b79ad017c7dda6b1b8d6debca1dd8e5
skrowll <lucasdejesus.rocha@hotmail.com>
2023-01-11 20:53:04 -0300

    refactor(Req. 2): :sparkles:

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,23 +1,23 @@
 class SimpleReport:
 
-    def oldest_manufacturing_date(list):
-        return min(product["data_de_fabricacao"] for product in list)
+    def oldest_manufacturing_date(products):
+        return min(product["data_de_fabricacao"] for product in products)
 
-    def nearest_expiration_date(list):
-        return min(product["data_de_validade"] for product in list)
+    def nearest_expiration_date(products):
+        return min(product["data_de_validade"] for product in products)
 
-    def get_unique_company_name(list):
+    def get_unique_company_name(products):
         companies_list = []
-        for product in list:
+        for product in products:
             if product["nome_da_empresa"] not in companies_list:
                 companies_list.append(product["nome_da_empresa"])
         return companies_list
 
-    def company_with_more_products(list, companies_list):
+    def company_with_more_products(products, companies_list):
         companies_dict = {}
         for company in companies_list:
             companies_dict[company] = 0
-        for product in list:
+        for product in products:
             for company in companies_list:
                 if company == product["nome_da_empresa"]:
                     companies_dict[company] = companies_dict[company] + 1
@@ -25,15 +25,15 @@ class SimpleReport:
         return max(companies_dict, key=companies_dict.get)
 
     @classmethod
-    def generate(self, list):
+    def generate(self, products):
 
-        companies_list = self.get_unique_company_name(list)
+        companies_list = self.get_unique_company_name(products)
 
         return (
             "Data de fabricação mais antiga: "
-            f"{self.oldest_manufacturing_date(list)}\n"
+            f"{self.oldest_manufacturing_date(products)}\n"
             "Data de validade mais próxima: "
-            f"{self.nearest_expiration_date(list)}\n"
+            f"{self.nearest_expiration_date(products)}\n"
             "Empresa com mais produtos: "
-            f"{self.company_with_more_products(list, companies_list)}"
+            f"{self.company_with_more_products(products, companies_list)}"
         )
