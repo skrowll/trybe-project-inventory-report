e42626aa278a8837846d9a38e8a7dc2f9d5e4de5
skrowll <lucasdejesus.rocha@hotmail.com>
2023-01-12 00:45:29 -0300

    feat(Req. 5): add function read_json in class Inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 import csv
+import json
 
 
 class Inventory:
@@ -14,7 +15,17 @@ class Inventory:
             elif report_type == "completo":
                 return CompleteReport.generate(list(data))
 
+    def read_json(path, report_type):
+        with open(path, mode="r") as json_file:
+            data = json.loads(json_file.read())
+            if report_type == "simples":
+                return SimpleReport.generate(list(data))
+            elif report_type == "completo":
+                return CompleteReport.generate(list(data))
+
     @classmethod
     def import_data(self, path, report_type):
         if "csv" in path:
             return self.read_csv(path, report_type)
+        elif "json" in path:
+            return self.read_json(path, report_type)
